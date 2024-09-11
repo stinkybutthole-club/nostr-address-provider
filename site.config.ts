bba10909e039ccf1155a0a4e010b645da883cea2
pcwd <41215821+pcwd@users.noreply.github.com>
2024-09-11 14:47:03 +1000

    Update site.config.ts

--- site.config.ts
+++ site.config.ts
@@ -1,16 +1,16 @@
 export default {
   // your domain name
-  host: "zaps.lol",
+  host: "stinkybutthole.club",
 
   // set to true to disable registration site-wide
   registrationDisabled: false,
 
   // site slogan
-  slogan: "Free nostr addresses.",
+  slogan: "Nostr NIP-05 vanity addresses.",
 
   // your donation LNURL
   donationLNUrl:
-    "LNURL1DP68GURN8GHJ7MR9VAJKUEPWD3HXY6T5WVHXXMMD9AKXUATJD3CZ7JMPVDV9YCC4FVREX",
+    "LNURL1DP68GURN8GHJ7MRWVF5HGUEWD3H8WCTVD3JHGTNPWPCZ7MRWW4EXCUP0FEXXW6JCD5PE9Y7N",
 
   // name of your MongoDB database
   dbName: "users",
